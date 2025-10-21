256f8d90c9afd126ed23183356d1f5413bb94356
Lokesh2482 <ce24b082@smail.iitm.ac.in>
2025-10-21 19:20:12 +0530

    class and object basics done

---
+++ oops1.py
@@ -0,0 +1,25 @@
+#How to initiate a class
+
+class employee:
+    #special method/magic method/dunder method-constructor
+    def __init__(self):
+        print("started executing attributes/data")
+        self.id = 123
+        self.salary = 50000
+        self.designation = "SDE" 
+        print("attributes or data have been initiated")
+
+    def travel(self,destination):
+        print("this travel function was called manually")
+        print(f"Employee is now travelling to {destination}")
+
+
+
+#create an obj/instance of the class
+sam = employee()
+#print(sam.salary)
+
+#calling a method
+#sam.travel("Mumbai")
+
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,11 @@
+lst = [1,2,3]
+my_str = "MLOps playlist"
+my_int = 155
+
+#print(type(my_int))
+my_str.capitalize()
+print(my_str)
+
+a = 'x'
+b = 'y'
+print(a+b)
